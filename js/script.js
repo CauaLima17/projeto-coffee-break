e31d9f7d8d8422bb45dbdac1d0eae7a559d79f08
Cauã de Lima <caua-l1@outlook.com>
2024-02-25 18:31:02 -0300

    página menu responsiva

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
-// Teste pagina sobre
+// Função de slide (Página Sobre)
+
+const sobre_slide_image_1 = document.querySelector('.sobre-cuidados-image');
+
+const sobre_Seta_Slide = document.querySelector('.sobre-cuidados svg');
 
 function sobreClickSlide(){
-    sobre_slide_image_1.classList.toggle('sobre-slide-movimento')
-    sobre_Seta_Slide.classList.toggle('slide-ativo')
+    sobre_slide_image_1.classList.toggle('sobre-slide-movimento');
+    sobre_Seta_Slide.classList.toggle('slide-ativo');
 }
 
-const sobre_slide_image_1 = document.querySelector('.sobre-cuidados-image')
+if (sobre_Seta_Slide) {
+    if (window.matchMedia("(min-width: 1200px)").matches) {
+        sobre_Seta_Slide.addEventListener('click', sobreClickSlide);
+    }
+}
 
-const sobre_Seta_Slide = document.querySelector('.sobre-cuidados svg')
 
-if (window.matchMedia("(min-width: 1200px)").matches) {
-    sobre_Seta_Slide.addEventListener('click', sobreClickSlide)
+// Função de filtro (Página menu)
+
+const botaoFiltrarMenu = document.querySelector('.menu-header button');
+
+const filtrosDoMenu = document.querySelector('.menu-header-filtros');
+
+function abrirFiltrosMenu () {
+    filtrosDoMenu.classList.toggle('display-block');
+}
+
+if (botaoFiltrarMenu) {
+    botaoFiltrarMenu.addEventListener('click', abrirFiltrosMenu);
 }
