47f2f47c90624e554740241c63764de2cd5e6fae
Cauã de Lima <caua-l1@outlook.com>
2024-04-11 21:08:42 -0300

    barra de pesquisa do menu concluida, mostrando produto escolhido do menu

--- js/script.js
+++ js/script.js
@@ -24,3 +24,50 @@ function showRespNav() {
         responsiveNav.style.display = 'none'
     };
 };
+
+// NAVEGAÇÃO PELO MENU (PESQUISA)
+const searchBox = document.getElementById('search-box');
+
+searchBox.addEventListener('input', () => {
+    let userSearch = searchBox.value;
+    let granParent;
+    const menuElementNames = document.querySelectorAll('.menu-item .menu-item-nome');
+
+    menuElementNames.forEach((element) => {
+        granParent = element.parentElement.parentElement;
+
+        if (!element.textContent.toLowerCase().includes(userSearch.toLowerCase())){
+            granParent.style.display = 'none';
+        } else {
+            granParent.style.display = 'grid';
+        };
+    });
+});
+
+// MOSTRANDO ELEMENTO ESCOLHIDO
+const menuItemButtons = document.querySelectorAll('.menu-item-conteudo button');
+const activeElement = document.getElementById('item-ativo');
+
+menuItemButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let granParent = button.parentElement.parentElement;
+        let productName = granParent.querySelector('.menu-item-nome').textContent;
+        let productDescription = granParent.querySelector('.menu-item-descricao').textContent;
+        let productPhoto = granParent.querySelector('.menu-item-image picture');
+
+        let activeElement__ProductName = activeElement.querySelector('.item-ativo-nome');
+        activeElement__ProductName.textContent = productName;
+
+        let activeElement__ProductDescription = activeElement.querySelector('.item-ativo-descricao');
+        activeElement__ProductDescription.textContent = productDescription;
+
+        let activeElement__ProductPhoto = activeElement.querySelector('.item-ativo-image');
+        activeElement__ProductPhoto.innerHTML = productPhoto.innerHTML;
+
+        activeElement.style.display = 'block';
+    });
+});
+
+function exitActiveElement(){
+    activeElement.style.display = 'none'
+};
